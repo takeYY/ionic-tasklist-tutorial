cfa5ad686795aa2f0eb145bd1dbbc3335065a5c4
RyoYasutake <ryasutake@landscape.co.jp>
2022-04-22 14:21:23 +0900

    ✨ (tasklist): タスクの削除と更新追加

--- src/app/tasklist/tasklist.page.ts
+++ src/app/tasklist/tasklist.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActionSheetController, AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-tasklist',
@@ -7,7 +8,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TasklistPage implements OnInit {
   tasks: { name: string }[] = [];
-  constructor() {}
+  constructor(
+    public actionSheetController: ActionSheetController,
+    public alertController: AlertController,
+  ) {}
 
   ngOnInit() {}
 
@@ -15,5 +19,83 @@ export class TasklistPage implements OnInit {
     if ('tasks' in localStorage) {
       this.tasks = JSON.parse(localStorage.getItem('tasks'));
     }
+  }
+
+  async changeTask(index: number) {
+    const actionSheet = await this.actionSheetController.create({
+      header: 'タスクの変更',
+      buttons: [
+        {
+          text: '削除',
+          role: 'destructive',
+          icon: 'trash',
+          handler: () => {
+            this._checkDelete(index);
+          },
+        },
+        {
+          text: '変更',
+          icon: 'create',
+          handler: () => {
+            this._renameTask(index);
+          },
+        },
+        {
+          text: '閉じる',
+          role: 'cancel',
+          icon: 'close',
+          handler: () => {
+            console.log('Cancel clicked');
+          },
+        },
+      ],
+    });
+    actionSheet.present();
+  }
+
+  async _checkDelete(index: number) {
+    const prompt = await this.alertController.create({
+      header: '本当に削除しますか？',
+      buttons: [
+        {
+          text: '閉じる',
+        },
+        {
+          text: '削除',
+          role: 'destructive',
+          handler: () => {
+            this.tasks.splice(index, 1);
+            localStorage.tasks = JSON.stringify(this.tasks);
+          },
+        },
+      ],
+    });
+    prompt.present();
+  }
+
+  async _renameTask(index: number) {
+    const prompt = await this.alertController.create({
+      header: '変更後のタスク',
+      inputs: [
+        {
+          name: 'task',
+          placeholder: 'タスク',
+          value: this.tasks[index].name,
+        },
+      ],
+      buttons: [
+        {
+          text: '閉じる',
+        },
+        {
+          text: '保存',
+          handler: (data) => {
+            this.tasks[index] = { name: data.task };
+            localStorage.tasks = JSON.stringify(this.tasks);
+          },
+        },
+      ],
+    });
+    prompt.present();
   }
 }
